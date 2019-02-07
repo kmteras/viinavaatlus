26eb34a96d23f7d35f314a2f5a40943c93a85b65
Aleksander Avastu <aleksander.avastu@gmail.com>
2019-02-07 08:09:21 +0200

    coop almost done

--- bin/scrapers/coop.js
+++ bin/scrapers/coop.js
@@ -9,13 +9,14 @@ class CoopScraper extends Scraper
         super("Coop");
         this.baseUrl = 'https://ecoop.ee/';
         this.categoryPages = [
-            {url: 'https://ecoop.ee/et/kategooriad/kange-alkohol/',             category: 'Strong alcohol'},
-            {url: 'https://ecoop.ee/et/kategooriad/punased-veinid/',            category: 'Red wine'},
-            {url: 'https://ecoop.ee/et/kategooriad/valged-veinid/',             category: 'White wine'},
-            {url: 'https://ecoop.ee/et/kategooriad/vahuveinid/',                category: 'Sparklin wine'},
-            {url: 'https://ecoop.ee/et/kategooriad/olled/',                     category: 'Beer'},
-            {url: 'https://ecoop.ee/et/kategooriad/lahjad-alkohoolsed-joogid/', category: 'Light beverages'},
-            {url: 'https://ecoop.ee/et/kategooriad/alkoholivabad-joogid/',      category: 'Non-alcoholic'}
+            // {url: 'https://ecoop.ee/et/kategooriad/kange-alkohol/',             category: 'Strong alcohol'},
+            // {url: 'https://ecoop.ee/et/kategooriad/punased-veinid/',            category: 'Red wine'},
+            // {url: 'https://ecoop.ee/et/kategooriad/valged-veinid/',             category: 'White wine'},
+            // {url: 'https://ecoop.ee/et/kategooriad/vahuveinid/',                category: 'Sparklin wine'},
+            // {url: 'https://ecoop.ee/et/kategooriad/olled/',                     category: 'Beer'},
+            // {url: 'https://ecoop.ee/et/kategooriad/lahjad-alkohoolsed-joogid/', category: 'Light beverages'},
+            // {url: 'https://ecoop.ee/et/kategooriad/alkoholivabad-joogid/',      category: 'Non-alcoholic'},
+            {url: ['https://ecoop.ee/api/v1/products?page=', '&category=58&ordering=popularity'], category: 'placeholder'}
         ];
     }
 
@@ -28,9 +29,33 @@ class CoopScraper extends Scraper
     }
 
     scrapeCategoryPage(category, callback) {
+        let index = 18;
+        let responseObjects = [];
+        populateResponseObject(url, index);
 
-        console.log(category);
-        console.log(callback);
 
     }
+
+    populateResponseObject(url, index) {
+        console.info('\nScraping: ' + category.url[0] + index + category.url[1]);
+        
+        let options = {
+            uri: category.url[0] + index + category.url[1],
+            headers: {
+                'User-Agent': 'Request-Promise'
+            },
+            json: true // Automatically parses the JSON string in the response
+        };
+
+        rp(options)
+            .then(function (response) {
+                responseObjects.push(response.results);
+
+                if (response.next != null) this.populateResponseObject(url, index + 1);
+            })
+            .catch(function (err) {
+                console.error('Connection to API failed');
+        });
+    }
 }
+module.exports = CoopScraper;
