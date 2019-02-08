78b51443c9215a3beb2fcf67688a1700e0611eb8
Karl Martin Teras <karl.martin@ter.as>
2019-02-08 23:45:40 +0200

    Cleaned up frontend javascript

--- public/js/viinavaatlus.js
+++ public/js/viinavaatlus.js
@@ -1,6 +1,3 @@
-var content = [
-];
-
 $(document).ready(function () {
     $('.ui.menu .ui.dropdown').dropdown({
         on: 'click'
@@ -44,32 +41,12 @@ $(document).ready(function () {
 
         })
     });
-    console.log(Cookies.get("age"));
-    if (Cookies.get("cookieconsent_status")=="allow" && Cookies.get("age") != 1) {
+
+    if (Cookies.get("cookieconsent_status") === "allow" && Cookies.get("age") !== "1") {
+        confirmation()
+    } else if (Cookies.get("cookieconsent_status") !== "allow") {
         confirmation()
     }
-    else if(Cookies.get("cookieconsent_status")!="allow"){
-        confirmation()
-    }
-
-    var ul, li, a;
-    ul = document.getElementById("alkoList");
-    console.log(ul);
-    li = ul.getElementsByTagName('a');
-
-
-    for(i=0;i<li.length;i++){
-        var map = {};
-        map['title']=li[i].textContent.toLowerCase();
-        content.push(map);
-    }
-    console.log(content);
-
-    $('.ui.search')
-        .search({
-            source: content
-        })
-    ;
 });
 
 function search() {
@@ -77,19 +54,17 @@ function search() {
 }
 
 function confirmation() {
-    $('.ui.basic.modal')
-        .modal('show')
-    ;
+    $('.ui.basic.modal').modal('show');
+
     document.getElementById("declineButton").onclick = function () {
-        Cookies.set("age", 0);
         location.href = "/limpa";
     };
+
     document.getElementById("confirmButton").onclick = function () {
-        if(Cookies.get("cookieconsent_status")=="allow"){
-            Cookies.set("age", 1);
-        }
-        else{
-            Cookies.set("age",0);
+        if (Cookies.get("cookieconsent_status") === "allow") {
+            Cookies.set("age", "1");
+        } else {
+            Cookies.set("age", "0");
         }
     }
 }
@@ -102,28 +77,5 @@ function removeEstonianLetters(string) {
     string = string.replace(/ü/g, "u");
     return string;
 }
-
-function searchFunction() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('searchBox');
-    filter = input.value.toLowerCase();
-    ul = document.getElementById("alkoList");
-    li = ul.getElementsByTagName('a');
-
-    for (i = 0; i < li.length; i++) {
-        txtValue = li[i].textContent.toLowerCase();
-        console.log(filter);
-        if (txtValue.indexOf(filter) > -1) {
-            li[i].style.display = "";
-            ul.style.display = "inline";
-        }else {
-            li[i].style.display = "none";
-        }
-        if(input.value==""){
-            li[i].style.display="none";
-        }
-    }
-}
-
 
 
