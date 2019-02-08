655de8defa81a2539399b86031b3993833735b22
Karl Martin Teras <karl.martin@ter.as>
2019-02-08 17:26:54 +0200

    Fixed searching

--- routes/search.js
+++ routes/search.js
@@ -4,19 +4,20 @@ const db = require('../bin/db');
 const productService = require('../bin/productService');
 
 router.get('/:search', (req, res, next) => {
-    db.getDb().collection("products").find({name: {$regex: req.params.search}}).toArray((err, result) => {
+    db.getDb().collection("products").find({name: {$regex: req.params.search}}).limit(10).toArray((err, result) => {
         if (err) {
             console.error(err);
             res.render('search', {products: null});
         }
 
         if (result.length === 0) {
-            db.getDb().collection("products").find({category: {$regex: req.params.search}}).toArray((err, result) => {
+            db.getDb().collection("products").find({category: {$regex: req.params.search}}).limit(10).toArray((err, result) => {
                 result = productService.prepareSearchResultsForRender(result);
                 res.render('search', {products: result});
             });
         } else {
             result = productService.prepareSearchResultsForRender(result);
+            console.log(result);
             res.render('search', {products: result});
         }
     });
