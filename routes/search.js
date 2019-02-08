b3e5cff75863d09b5f9feadbf171259b7dbdd9e1
Karl Martin Teras <karl.martin@ter.as>
2019-02-08 23:35:26 +0200

    Improved search results by adding no products warning and keeping search value

--- routes/search.js
+++ routes/search.js
@@ -4,24 +4,24 @@ const db = require('../bin/db');
 const productService = require('../bin/productService');
 
 router.get('/', (req, res, next) => {
-
+    res.render('search', {products: null, search: req.params.search});
 });
 
 router.get('/:search', (req, res, next) => {
     db.getDb().collection("products").find({name: {$regex: req.params.search}}).limit(10).toArray((err, result) => {
         if (err) {
             console.error(err);
-            res.render('search', {products: null});
+            res.render('search', {products: null, search: req.params.search});
         }
 
         if (result.length === 0) {
             db.getDb().collection("products").find({category: {$regex: req.params.search}}).limit(10).toArray((err, result) => {
                 result = productService.prepareSearchResultsForRender(result);
-                res.render('search', {products: result});
+                res.render('search', {products: result, search: req.params.search});
             });
         } else {
             result = productService.prepareSearchResultsForRender(result);
-            res.render('search', {products: result});
+            res.render('search', {products: result, search: req.params.search});
         }
     });
 });
